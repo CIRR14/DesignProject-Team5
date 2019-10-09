594931a96569cd717d2bb041a2e0ee5f60d9c383
Ivan Romero <ivanrr308@gmail.com>
2019-10-08 19:54:54 -0500

    Nav component and routing

---
+++ frontend/src/app/components/my-nav/my-nav.component.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './../auth/auth.service';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-my-nav',
+  templateUrl: './my-nav.component.html',
+  styleUrls: ['./my-nav.component.scss']
+})
+export class MyNavComponent{
+  profilePicture = '../../../assets/profile/default-profile.png';
+  title = 'Garage Designs of St. Louis Management System';
+
+
+    constructor(private breakpointObserver: BreakpointObserver,
+                private authService: AuthService) {}
+
+
+    isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+        .pipe(
+            map(result => result.matches)
+      );
+
+
+
+}
