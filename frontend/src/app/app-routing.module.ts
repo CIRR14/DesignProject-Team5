8e8578be274489f689aec68d7649b5d194862904
Ivan Romero <ivanrr308@gmail.com>
2019-10-08 19:50:41 -0500

    App routing and component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
+import { ExportComponent } from './components/export/export.component';
+import { AvailabilitiesComponent } from './components/availabilities/availabilities.component';
+import { AuthGuard } from './components/auth/auth.guard';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DashComponent } from './components/dash/dash.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeesComponent } from './components/employees/employees.component';
 
-const routes: Routes = [];
+
+
+
+const appRoutes: Routes = [
+  {
+    path: 'dash',
+    component: DashComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'employees',
+    component: EmployeesComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'availabilities',
+    component: AvailabilitiesComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'jobs',
+    loadChildren: './components/jobs/jobs.module#JobsModule'
+    // Lazy Loading
+  },
+  {
+    path: 'export',
+    component: ExportComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/dash',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+  ];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,49 @@
+import { AppMaterialModule } from './shared/app-material.module';
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './components/my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { DashComponent } from './components/dash/dash.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthModule } from './components/auth/auth.module';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { AvailabilitiesComponent } from './components/availabilities/availabilities.component';
+import { ExportComponent } from './components/export/export.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent,
+    DashComponent,
+    NotFoundComponent,
+    EmployeesComponent,
+    AvailabilitiesComponent,
+    ExportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AuthModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    LayoutModule,
+    FormsModule,
+    AngularFireDatabaseModule,
+    AppMaterialModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
