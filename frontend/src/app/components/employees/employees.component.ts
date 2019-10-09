b1ec6a12dfdf31e750baf0d7266ea714e19c5203
Ivan Romero <ivanrr308@gmail.com>
2019-10-08 19:53:03 -0500

    Employees component created

---
+++ frontend/src/app/components/employees/employees.component.ts
@@ -0,0 +1,18 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.scss']
+})
+export class EmployeesComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+
+  }
+
+
+}
