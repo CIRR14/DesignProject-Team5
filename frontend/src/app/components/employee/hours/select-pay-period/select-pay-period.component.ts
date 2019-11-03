a087db02c4009d634c900c1347bc7be9d1693376
Sami <smabp3@gmail.com>
2019-11-03 17:18:47 -0600

    js to ts
    update

--- frontend/src/app/components/employee/hours/select-pay-period/select-pay-period.component.ts
+++ frontend/src/app/components/employee/hours/select-pay-period/select-pay-period.component.ts
@@ -7,57 +7,56 @@ import * as moment from 'moment';
   styleUrls: ['./select-pay-period.component.scss']
 })
 export class SelectPayPeriodComponent implements OnInit {
-  static controller(arg0: string, arg1: ($scope: any, usersApi: any, clockApi: any, payperiodFactory: any, totaltimeFactory: any) => void) {
-    throw new Error("Method not implemented.");
+
+  constructor() {
+    setInterval(() => {
+    const currentDate = new Date();
+    this.datemessage = currentDate.toDateString();
+    } );
+
+    setInterval(() => {
+    const currentTime = new Date();
+    this.timemessage = currentTime.toLocaleTimeString();
+    } , 1000);
+
   }
 
   datemessage: string;
   timemessage: string;
-
-  constructor() { 
-    setInterval(() =>
-    {
-    let currentDate = new Date();
-    this.datemessage = currentDate.toDateString()
-    } );
-
-    setInterval(() =>
-    {
-    let currentTime = new Date();
-    this.timemessage = currentTime.toLocaleTimeString();
-    } ,1000);
-    
+  static controller(arg0: string, arg1: ($scope: any, usersApi: any, clockApi: any, payperiodFactory: any, totaltimeFactory: any) => void) {
+    throw new Error('Method not implemented.');
   }
 
   ngOnInit() {
-    
+
 SelectPayPeriodComponent.controller('edit', function edit($scope, usersApi, clockApi, payperiodFactory, totaltimeFactory) {
   $scope.currentTimes = {};
   $scope.currentUser = 0;
   $scope.selectedDate = moment();
-  $scope.startDate = "";
-  $scope.endDate = "";
-  $scope.startTime = "";
-  $scope.endTime ="";
+  $scope.startDate = '';
+  $scope.endDate = '';
+  $scope.startTime = '';
+  $scope.endTime = '';
 
 
-  //get the list of users
-  usersApi.getActive().then(function(response) {
+  // get the list of users
+  usersApi.getActive().then((response) =>  {
       $scope.users = response.data;
-      
+
   });
   function getTimes(date) {
-    var object;
+    let object;
+
     object.payperiodTotal = 0;
-    var periodDates = payperiodFactory.periodDates(date);
+    const periodDates = payperiodFactory.periodDates(date);
     $scope.startDate = periodDates.firstWeekStart;
     $scope.endDate = periodDates.secondWeekEnd;
-    clockApi.get($scope.currentUser, periodDates.firstWeekStart, periodDates.firstWeekEnd).then(function(response) {
+    clockApi.get($scope.currentUser, periodDates.firstWeekStart, periodDates.firstWeekEnd).then((response) =>  {
         object.firstWeekTotal = totaltimeFactory.getTotal(response.data);
         object.firstWeek = response.data;
         object.payperiodTotal += object.firstWeekTotal;
     });
-    clockApi.get($scope.currentUser, periodDates.secondWeekStart, periodDates.secondWeekEnd).then(function(response) {
+    clockApi.get($scope.currentUser, periodDates.secondWeekStart, periodDates.secondWeekEnd).then((response) =>  {
         object.secondWeekTotal = totaltimeFactory.getTotal(response.data);
         object.secondWeek = response.data;
         object.payperiodTotal += object.secondWeekTotal;
@@ -68,42 +67,42 @@ SelectPayPeriodComponent.controller('edit', function edit($scope, usersApi, cloc
       return object;
     }
 }
-    //resets back to basics
-    function reset() {
+    // resets back to basics
+  function reset() {
       $scope.currentTimes = {};
       $scope.currentUser = 0;
       $scope.selectedDate = moment();
-      $scope.startDate = "";
-      $scope.endDate = "";
-      $scope.startTime = "";
-      $scope.endTime = "";
+      $scope.startDate = '';
+      $scope.endDate = '';
+      $scope.startTime = '';
+      $scope.endTime = '';
   }
 
-  $scope.getTimes = function() {
-      if($scope.currentUser > 0) {
+  $scope.getTimes = () => {
+      if ($scope.currentUser > 0) {
           $scope.currentTimes = getTimes($scope.selectedDate);
       } else {
           reset();
       }
   };
 
-  $scope.addRow = function() {
-      var start = moment($scope.selectedDate + " " + $scope.startTime, "YYYY-MM-DD h:mm a").format("YYYY-MM-DD HH:mm:ss");
-      var end = moment($scope.selectedDate + " " + $scope.endTime, "YYYY-MM-DD h:mm a").format("YYYY-MM-DD HH:mm:ss");
-      clockApi.add($scope.currentUser, start, end).then(function() {
+  $scope.addRow = () => {
+      const start = moment($scope.selectedDate + ' ' + $scope.startTime, 'YYYY-MM-DD h:mm a').format('YYYY-MM-DD HH:mm:ss');
+      const end = moment($scope.selectedDate + ' ' + $scope.endTime, 'YYYY-MM-DD h:mm a').format('YYYY-MM-DD HH:mm:ss');
+      clockApi.add($scope.currentUser, start, end).then(() => {
           $scope.currentTimes = getTimes($scope.selectedDate);
-          $scope.startTime = "";
-          $scope.endTime = "";
+          $scope.startTime = '';
+          $scope.endTime = '';
       });
   };
 
-  $scope.removeRow = function(id) {
-      clockApi.remove(id).then(function() {
+  $scope.removeRow = (id) => {
+      clockApi.remove(id).then(() => {
           $scope.currentTimes = getTimes($scope.selectedDate);
       });
   };
 
-  $scope.reset = function() {
+  $scope.reset = () => {
       reset();
   };
 });
