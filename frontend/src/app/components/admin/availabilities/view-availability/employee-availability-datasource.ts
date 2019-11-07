cff1905f539cf7d24a673e4884afd0c4053b9e08
Ivan Romero <>
2019-11-07 09:14:07 -0600

    admin-availability calendar to show everyone's available dates

---
+++ frontend/src/app/components/admin/availabilities/view-availability/employee-availability-datasource.ts
@@ -0,0 +1,51 @@
+import { MatPaginator, MatSort } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
+import { map } from 'rxjs/operators';
+import { Observable, of as observableOf, merge } from 'rxjs';
+
+export interface EmployeeAvailabilityItem {
+  empId: string;
+  title: string;
+  date: Date[];
+}
+
+export class EmployeeAvailabilityDataSource extends DataSource<EmployeeAvailabilityItem> {
+  empAvailability: EmployeeAvailabilityItem[] = [{
+    empId: '',
+    title: '',
+    date: []
+  }];
+
+  constructor(private paginator: MatPaginator, private sort: MatSort) {
+    super();
+  }
+
+  connect(): Observable<EmployeeAvailabilityItem[]> {
+    const dataMutations = [
+      observableOf(this.empAvailability),
+      this.paginator.page,
+      this.sort.sortChange
+    ];
+
+    this.paginator.length = this.empAvailability.length;
+
+    return merge(...dataMutations).pipe(map(() => {
+      return this.getPagedData(this.getSortedData([...this.empAvailability]));
+    }));
+  }
+
+
+  disconnect() {
+  }
+
+  private getPagedData(data: EmployeeAvailabilityItem[]) {
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+    return data.splice(startIndex, this.paginator.pageSize);
+  }
+
+  private getSortedData(data: EmployeeAvailabilityItem[]) {
+    if (!this.sort.active || this.sort.direction === '') {
+      return data;
+    }
+}
+}
--- frontend/src/app/components/employee/employee-availability/employee-availability.component.ts
+++ frontend/src/app/components/employee/employee-availability/employee-availability.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './../../auth/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-employee-availability',
@@ -13,13 +16,19 @@ export class EmployeeAvailabilityComponent implements OnInit {
   public multiSelect = true;
   public dateValue: Date = new Date(this.fullYear, this.month , 11);
   public minDate: Date = new Date(this.fullYear, this.month, this.today + 1);
+  public maxDate: Date = new Date(this.fullYear, this.month, 31);
 
   availableDates = [];
   selectedDate;
+  currentUser: firebase.User;
+  currentDate: Date = new Date();
+  currentMonth = this.currentDate.getMonth() + 1;
 
-  constructor() { }
+
+  constructor(private afs: AngularFirestore, private authService: AuthService) { }
 
   ngOnInit() {
+    this.currentUser = this.authService.currentUser;
   }
 
 
@@ -32,6 +41,19 @@ export class EmployeeAvailabilityComponent implements OnInit {
       console.log(selectedDate);
 
     });
+  }
+
+  submit() {
+    const availabilityRef = this.afs.doc(`users/${this.currentUser.uid}/notAvailable/${this.currentMonth}`);
+
+    const data = {
+      title: this.currentUser.displayName,
+      userId: this.currentUser.uid,
+      notAvailable: this.availableDates
+    };
+
+    availabilityRef.set(data, {merge: true});
+
   }
 
 
