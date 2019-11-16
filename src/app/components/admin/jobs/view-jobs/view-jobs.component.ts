ab0d8d9599931d8a0b027486f1f11669130bc155
Ivan Romero <>
2019-11-16 17:59:28 -0600

    Merge master into hosting-merger

--- src/app/components/admin/jobs/view-jobs/view-jobs.component.ts
+++ src/app/components/admin/jobs/view-jobs/view-jobs.component.ts
@@ -23,7 +23,7 @@ export class ViewJobsComponent implements OnInit, OnDestroy{
   dataSource: ViewjobsDataSource; 
 
 // private jobs$: Observable<Job[]>;
- displayedColumns = ['created', 'clientName', 'address', 'status'];
+ displayedColumns = ['created', 'clientName', 'address'];
  subscription: Subscription;
 
 constructor(
