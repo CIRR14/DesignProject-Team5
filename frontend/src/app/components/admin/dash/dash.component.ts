560f06eb6b3797f1e671dd2dfd5c04b691546ed5
Ivan Romero <>
2019-11-16 17:30:16 -0600

    Dashboard and be able to export for the payroll based on payPeriod

--- frontend/src/app/components/admin/dash/dash.component.ts
+++ frontend/src/app/components/admin/dash/dash.component.ts
@@ -1,8 +1,17 @@
 import { Observable, Subscription } from 'rxjs';
 import { User } from './../../auth/user';
 import { AuthService } from '../../auth/auth.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollectionGroup, AngularFirestoreCollection } from '@angular/fire/firestore';
+
+import * as XLSX from 'xlsx';
+
+export interface PayrollElement {
+  name: string;
+  rate: number;
+  hours: number;
+  uid: string;
+}
 
 
 @Component({
@@ -12,43 +21,112 @@ import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firest
 })
 export class DashComponent implements OnInit, OnDestroy {
 
-  postRef: AngularFirestoreDocument<any>;
-  post$: Observable<any>;
-  user: User;
-  subscription: Subscription;
+  payPeriodRef: AngularFirestoreCollectionGroup<any>;
+  payPeriod$: Observable<any>;
+  payPeriodData = [];
 
-  documentId = 'testPost';
+  usersRef: AngularFirestoreCollection<any>;
+  user$: Observable<any>;
+  userData = [];
+
+  loggedInUser: User;
+  loggedInUserSubscription: Subscription;
+
+  payrollData = [];
+  displayedColumns: string[] = ['name', 'rate', 'hours', 'total'];
+  dataSource = this.payrollData;
+
+  currentDate = new Date();
+  currentMonth = new Date().getMonth() + 1;
+  currentPayPeriod: string;
 
   constructor(private afs: AngularFirestore, public auth: AuthService) { }
 
-  ngOnInit() {
-    this.postRef = this.afs.doc(`examplePost/${this.documentId}`);
-    this.post$ = this.postRef.valueChanges();
+  @ViewChild('TABLE') table: ElementRef;
 
-    this.subscription = this.auth.user$.subscribe(user => this.user = user);
+  ngOnInit() {
+    this.payPeriodRef = this.afs.collectionGroup(`payPeriod`);
+    this.payPeriod$ = this.payPeriodRef.valueChanges();
+
+    this.usersRef = this.afs.collection('users');
+    this.user$ = this.usersRef.valueChanges();
+
+    this.loggedInUserSubscription = this.auth.user$.subscribe(user => {
+      this.loggedInUser = user;
+    });
+
+    this.getInfo();
+  }
+
+  async getInfo() {
+    await this.getAllEmployees();
+    await this.getAllPayPeriods();
+    this.matchEmpAndPayPeriod();
+  }
+
+  exportAsExcel() {
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'payroll');
+    XLSX.writeFile(wb, 'PayrollReport.xlsx');
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.loggedInUserSubscription.unsubscribe();
   }
 
-  editPost() {
-    if (this.auth.canUpdate(this.user)) {
-      this.postRef.update({title: 'Edited Title!'});
-    }
+  getCurrentPayPeriod(endDate, startDate) {
+    endDate = endDate.toLocaleDateString();
+    startDate = startDate.toLocaleDateString();
+
+    return `${startDate} - ${endDate}`;
   }
 
-  createPost() {
-    if (this.auth.canCreate(this.user)) {
-      this.postRef.set({
-        title: 'hello',
-        content: 'this is content'
+  getAllEmployees() {
+    return new Promise((resolve) => {
+      this.user$.subscribe((users) => {
+        users.forEach(user => {
+          const data = {
+            uid: user.uid,
+            name: user.displayName,
+            rate: user.hourlyRate,
+          };
+          this.userData.push(data);
+          resolve();
+        });
       });
-    }
+    });
   }
 
-  deletePost() {
-    this.postRef.delete();
+  getAllPayPeriods() {
+    return new Promise((resolve) => {
+      this.payPeriod$.subscribe((payPeriods) => {
+        payPeriods.forEach((payPeriod) => {
+          const pPEndDate = new Date(payPeriod.endDate.seconds * 1000);
+          const pPStartDate = new Date(payPeriod.startDate.seconds * 1000);
+          this.currentPayPeriod = this.getCurrentPayPeriod(pPEndDate, pPStartDate);
+          if (this.currentDate > pPStartDate && this.currentDate <= pPEndDate) {
+            this.payPeriodData.push(payPeriod);
+            resolve();
+          }
+        });
+      });
+    });
   }
 
+  matchEmpAndPayPeriod() {
+    this.userData.forEach((user) => {
+      this.payPeriodData.forEach((payP) => {
+        if (user.uid === payP.uid) {
+          const data: PayrollElement = {
+            name: user.name,
+            rate: user.rate,
+            hours: payP.hours,
+            uid: user.uid
+          };
+          this.payrollData.push(data);
+        }
+      });
+    });
+  }
 }
